91b8202925ba58fa5e1dbeaecaf47f7f6e637ebc
Emanoel Brunio <brunio700@gmail.com>
2023-02-24 14:24:37 -0300

    adicionando arrays de novas temporadas

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,6 +1,276 @@
-const t1 = [
+const s1 = [
     {
         title: 'Adeus, passado',
+        duration: 67
+    },
+    {
+        title: 'Estranhas',
+        duration: 45
+    },
+    {
+        title: 'Vá falar com sapos',
+        duration: 45
+    },
+    {
+        title: 'Hermanos',
+        duration: 45
+    },
+    {
+        title: 'Wildfire',
+        duration: 45
+    },
+    {
+        title: 'IT-19',
+        duration: 45
+    },
+]
+
+const s2 = [
+    {
+        title: 'tempo2',
+        duration: 67
+    },
+    {
+        title: 'Estranhas',
+        duration: 45
+    },
+    {
+        title: 'Vá falar com sapos',
+        duration: 45
+    },
+    {
+        title: 'Hermanos',
+        duration: 45
+    },
+    {
+        title: 'Wildfire',
+        duration: 45
+    },
+    {
+        title: 'IT-19',
+        duration: 45
+    },
+]
+
+const s3 = [
+    {
+        title: 'tempo3',
+        duration: 67
+    },
+    {
+        title: 'Estranhas',
+        duration: 45
+    },
+    {
+        title: 'Vá falar com sapos',
+        duration: 45
+    },
+    {
+        title: 'Hermanos',
+        duration: 45
+    },
+    {
+        title: 'Wildfire',
+        duration: 45
+    },
+    {
+        title: 'IT-19',
+        duration: 45
+    },
+]
+
+const s4 = [
+    {
+        title: 'tempo4',
+        duration: 67
+    },
+    {
+        title: 'Estranhas',
+        duration: 45
+    },
+    {
+        title: 'Vá falar com sapos',
+        duration: 45
+    },
+    {
+        title: 'Hermanos',
+        duration: 45
+    },
+    {
+        title: 'Wildfire',
+        duration: 45
+    },
+    {
+        title: 'IT-19',
+        duration: 45
+    },
+]
+
+const s5 = [
+    {
+        title: 'tempo5',
+        duration: 67
+    },
+    {
+        title: 'Estranhas',
+        duration: 45
+    },
+    {
+        title: 'Vá falar com sapos',
+        duration: 45
+    },
+    {
+        title: 'Hermanos',
+        duration: 45
+    },
+    {
+        title: 'Wildfire',
+        duration: 45
+    },
+    {
+        title: 'IT-19',
+        duration: 45
+    },
+]
+
+const s6 = [
+    {
+        title: 'tempo6',
+        duration: 67
+    },
+    {
+        title: 'Estranhas',
+        duration: 45
+    },
+    {
+        title: 'Vá falar com sapos',
+        duration: 45
+    },
+    {
+        title: 'Hermanos',
+        duration: 45
+    },
+    {
+        title: 'Wildfire',
+        duration: 45
+    },
+    {
+        title: 'IT-19',
+        duration: 45
+    },
+]
+
+const s7 = [
+    {
+        title: 'tempo7',
+        duration: 67
+    },
+    {
+        title: 'Estranhas',
+        duration: 45
+    },
+    {
+        title: 'Vá falar com sapos',
+        duration: 45
+    },
+    {
+        title: 'Hermanos',
+        duration: 45
+    },
+    {
+        title: 'Wildfire',
+        duration: 45
+    },
+    {
+        title: 'IT-19',
+        duration: 45
+    },
+]
+
+const s8 = [
+    {
+        title: 'tempo8',
+        duration: 67
+    },
+    {
+        title: 'Estranhas',
+        duration: 45
+    },
+    {
+        title: 'Vá falar com sapos',
+        duration: 45
+    },
+    {
+        title: 'Hermanos',
+        duration: 45
+    },
+    {
+        title: 'Wildfire',
+        duration: 45
+    },
+    {
+        title: 'IT-19',
+        duration: 45
+    },
+]
+
+const s9 = [
+    {
+        title: 'tempo9',
+        duration: 67
+    },
+    {
+        title: 'Estranhas',
+        duration: 45
+    },
+    {
+        title: 'Vá falar com sapos',
+        duration: 45
+    },
+    {
+        title: 'Hermanos',
+        duration: 45
+    },
+    {
+        title: 'Wildfire',
+        duration: 45
+    },
+    {
+        title: 'IT-19',
+        duration: 45
+    },
+]
+
+const s10 = [
+    {
+        title: 'tempo10',
+        duration: 67
+    },
+    {
+        title: 'Estranhas',
+        duration: 45
+    },
+    {
+        title: 'Vá falar com sapos',
+        duration: 45
+    },
+    {
+        title: 'Hermanos',
+        duration: 45
+    },
+    {
+        title: 'Wildfire',
+        duration: 45
+    },
+    {
+        title: 'IT-19',
+        duration: 45
+    },
+]
+
+const s11 = [
+    {
+        title: 'tempo11',
         duration: 67
     },
     {
@@ -89,15 +359,17 @@ const persons = [
    
 ]
 
-let divEps = document.querySelector('.episodes');
+window.addEventListener('load', function(){
+    createList(s1, 1);
+});
 
 // função que retorna a string HTML do episódio
-function eps(i, title, duration) {
+function eps(t, e, title, duration) {
     return (
         `
-        <img src="assets/imagens/temporadas/t1e${i}.jpg" alt="">
+        <img src="assets/imagens/temporadas/t${t}e${e}.jpg" alt="">
         <div class="divTitles">
-            <h4 class="titleEps">${i}. ${title}</h4>
+            <h4 class="titleEps">${e}. ${title}</h4>
             <h4 class="duration">${duration} min.</h4>
         </div>
         <button class="cta">Assistir</button>
@@ -105,12 +377,73 @@ function eps(i, title, duration) {
     )
 }
 
-for (let i = 1; i <= 6; i++) {
-  const item = document.createElement("li");
-  item.classList.add('eps');
-  item.innerHTML = eps(i, t1[i-1].title, t1[i-1].duration);
-  divEps.appendChild(item);
+function createList(season, nSeason){
+    let divEps = document.querySelector('.episodes');
+    divEps.innerHTML = '';
+     
+    let e = 1;
+    season.forEach((item)=> {
+        const li = document.createElement("li");
+        li.classList.add('eps');
+
+        li.innerHTML = eps(nSeason, e, item.title, item.duration);
+        divEps.appendChild(li); 
+        e++;
+    });
+    // for (let i = 1; i <= t.length; i++) {
+    //     const item = document.createElement("li");
+    //     item.classList.add('eps');
+    //     item.innerHTML = eps(i, t[i-1].title, t[i-1].duration);
+    //     divEps.appendChild(item);
+    // }
 }
+
+
+const selected = document.querySelector('.select');
+selected.addEventListener('change', function() {
+    const selectedValue = selected.value;
+
+    switch(selectedValue){
+        case 's1':
+            createList(s1, 1);
+            break;
+        case 's2':
+            createList(s2, 2);
+            break;
+        case 's3':
+            createList(s3, 3);
+            break;
+        case 's4':
+            createList(s4, 4);
+            break;
+        case 's5':
+            createList(s5, 5);
+            break;
+        case 's6':
+            createList(s6, 6);
+            break;
+        case 's7':
+            createList(s7, 7);
+            break;
+        case 's8':
+            createList(s8, 8);
+            break;
+        case 's9':
+            createList(s9, 9);
+            break;
+        case 's10':
+            createList(s10, 10);
+            break;
+        case 's11':
+            createList(s11, 11);
+            break;
+        default: 
+            console.log('erro');        
+    }
+});
+
+
+
 
 
 
